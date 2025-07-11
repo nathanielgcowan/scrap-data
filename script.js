6d92c74d4aad9091629f2b56c9ec1ce19b23eddf
Nathaniel Cowan <nathaniel.g.cowan@gmail.com>
2025-07-11 16:27:36 -0500

    studied recursion

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 const person = {
     name: 'Kyle',
     friend: {
-        name: 'Joe',
+        name: "Joe",
         friend: {
             name: 'Sally'
         }
     }
 }
+
+function printNames(currentPerson) {
+    if (currentPerson == null) return
+    console.log(currentPerson.name)
+    printNames(currentPerson.friend)
+}
+
+printNames(person)
