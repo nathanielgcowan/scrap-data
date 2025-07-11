89453dbcb9a5709d90489190e4674e13f5a385ae
Nathaniel Cowan <nathaniel.g.cowan@gmail.com>
2025-07-11 16:10:26 -0500

    while loop

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-for (let i = 0; i < 1; i++) {
-    console.log('Test')
-    break;
+const person = {
+    name: 'Kyle',
+    friend: {
+        name: 'Joe',
+        friend: {
+            name: 'Sally'
+        }
+    }
 }
-
-// Create a for loop that loops from 0 to 10 and prints out all values from 0 to 10
-for (let i=0; i < 11; i++) {
-    console.log(i)
-    if (i === 5) break
-}
-// Modify this loop and exit the loop when the value is equal to 5 by using break
