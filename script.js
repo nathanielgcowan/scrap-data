6852631f266c5b81cddb9dfb743da84ef34aec31
Nathaniel Cowan <nathaniel.g.cowan@gmail.com>
2025-07-10 19:22:59 -0500

    work on the todo llist

--- script.js
+++ script.js
@@ -6,5 +6,16 @@ const input = document.querySelector('#item-input')
 // 2. When I submit the form add a new element
 form.addEventListener('#submit', e => {
     e.preventDefault();
+    
+    // 1. Create a new item
+    const item = document.createElement('div')
+    item.innerText = input.value
+    item.classList.add('list-item')
+
+    console.log(item)
+
+    // 2. Add that item to the list
+    // 3. Clear input
+    // 4. Setup event listener to deleter item when clicked
     console.log(input.value)
 })
