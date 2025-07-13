0d0a3f29d9b5ae0e8f8d88b421752fe464110139
Nathaniel Cowan <nathaniel.g.cowan@gmail.com>
2025-07-13 00:08:23 -0500

    event delegation

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-fdsf
+const buttons = document.querySelectorAll("button")
+
+document.addEventListener('click', () => {
+    console.log("Clicked Document")
+}, { capture: true })
+
+document.body.addEventListener('click', () => {
+    console.log("Clicked Body")
+}, { capture: true })
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        console.log("Clicked Button")
+    })
+})
